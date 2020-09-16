e88de39c6ee2a44db3cbf1ff13b6d946f1ede45a
ghudner <58570665+ghudner@users.noreply.github.com>
2020-09-16 10:07:54 -0600

    df

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,16 +11,13 @@ class Quiz extends Component {
     }
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
-        let endComponent;
-        if (isQuizEnd)
-            endComponent = <QuizEnd />
-        else
-            endComponent = <div></div>
 
         return (
             <div>
-                {endComponent}
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                {isQuizEnd ?
+                    <QuizEnd /> : 
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                    }
             </div>
                 )
     }
